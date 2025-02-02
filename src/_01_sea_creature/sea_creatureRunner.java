68c42d1c3f2bab9ebacd143fd013dc2f0d1915fc
league <league@adminisorsimac3.attlocal.net>
2025-02-01 16:28:14 -0800

    I kind of started

---
+++ src/_01_sea_creature/sea_creatureRunner.java
@@ -0,0 +1,15 @@
+package _01_sea_creature;
+
+public class sea_creatureRunner {
+	String spongeBob() {
+		return null;
+		
+			
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
